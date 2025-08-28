0eb1497eb3af9fb76ab566bf4e978d272a928b2e
Edinson Gutierrez <=>
2025-08-28 18:07:29 -0500

    fix: apply ruff format to the all project

--- medicals/serializers.py
+++ medicals/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from .models import Medical, Department, MedicalAvailability, MedicalNote
 
+
 class MedicalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medical
-        fields = '__all__'
+        fields = "__all__"
+
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = '__all__'
+        fields = "__all__"
+
 
 class MedicalAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalAvailability
-        fields = '__all__'
+        fields = "__all__"
+
 
 class MedicalNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalNote
-        fields = '__all__'
+        fields = "__all__"
