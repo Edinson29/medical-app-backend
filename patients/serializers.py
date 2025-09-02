eff43b11709a3d6806a9b02cea69d142efeaec11
Edinson Gutierrez <edinsongutierrez29@gmail.com>
2025-09-02 13:19:47 -0500

    fix: add fields per fields in serializers

--- patients/serializers.py
+++ patients/serializers.py
@@ -5,16 +5,25 @@ from .models import Patient, Insurance, MedicalRecord
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = "__all__"
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "contact_number",
+            "email",
+            "address",
+            "medical_history",
+        )
 
 
 class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Insurance
-        fields = "__all__"
+        fields = ("id", "patient", "provider", "policy_number", "expiration_date")
 
 
 class MedicalRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalRecord
-        fields = "__all__"
+        fields = ("id", "patient", "date", "diagnosis", "treatment", "follow_up_date")
