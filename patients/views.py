267ce0b8b954da7464318ea4069bed0e1d0c9a76
Edinson Gutierrez <edison.gutierrez-ext@ab-inbev.com>
2025-10-16 21:14:03 -0500

    feat: finish patients crud

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def list_patients(request):
+    if request.method == "GET":
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PatientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    
+
+@api_view(["GET", "PUT", "DELETE"])
+def detail_patient(request, pk):
+    try:
+        patient = Patient.objects.get(id=pk)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PatientSerializer(patient, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    elif request.method == "DELETE":
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
